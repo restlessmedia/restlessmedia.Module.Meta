7c730f70b04cb3578f306e862ff58f8869997f1c
Philip Cooper <phillycooper@hotmail.com>
2020-05-04 22:07:27 +0100

    Adding udt

---
+++ src/Data/UDTMeta.cs
@@ -0,0 +1,41 @@
+﻿using restlessmedia.Module.Data.Sql;
+
+namespace restlessmedia.Module.Meta.Data
+{
+  public class UDTMeta : UDTBase
+  {
+    public UDTMeta(int id, object value)
+    {
+      Id = id;
+      Value = value != null ? value.ToString() : null;
+    }
+
+    [MetaData("Id", System.Data.SqlDbType.Int)]
+    public int Id
+    {
+      get
+      {
+        return DataRecord.GetInt32(0);
+      }
+      set
+      {
+        DataRecord.SetValue(0, value);
+      }
+    }
+
+    [MetaData("Value", System.Data.SqlDbType.VarChar)]
+    public string Value
+    {
+      get
+      {
+        return DataRecord.GetString(1);
+      }
+      set
+      {
+        DataRecord.SetValue(1, value);
+      }
+    }
+
+    public const string TypeName = "UDTMeta";
+  }
+}
